16aaa55f83995844a766b6f8125362fe3fbe1d59
Chris Wheatley <chriswheatley87@gmail.com>
2015-02-14 11:43:29 +0000

    module scaffold

--- index.js
+++ index.js
@@ -1,7 +1,47 @@
-angular.module('pouchDemo', [])
+(function() {
+  
+  'use strict';
+  
+  runBootstrap.$inject = ['userSettings'];
 
-.controller('pouchCtrl', function() {
-  this.foo = {
-    bar: 'yo'
+  function runBootstrap(userSettings) {
+    
+    function dbSetup() {
+      var db = new PouchDB('foo');
+      
+      db.info().then(function (info) {
+        console.log('pouchDB info: ', info);
+      })
+    }
+    
+    userSettings.get();
+    dbSetup();
   }
-});
+  
+  function userSettings() {
+    function get() {
+      // get settings
+    }
+    
+    function put(data) {
+      // put settings
+    }
+    
+    return {
+      get: get,
+      put: put
+    }
+  }
+  
+  function pouchCtrl() {
+    function saveSetting(data) {
+      // save settings using userSettings.put
+    }
+  };
+
+  angular.module('pouchDemo', [])
+    .run(runBootstrap)
+    .factory('userSettings', userSettings)
+    .controller('pouchCtrl', pouchCtrl);
+
+})();
