73c531976ced4300417776a63a62eb86a21faa50
Chris Wheatley <chriswheatley87@gmail.com>
2015-02-15 23:29:28 +0000

    add and update documents

--- index.js
+++ index.js
@@ -2,29 +2,28 @@
   
   'use strict';
   
-  runBootstrap.$inject = ['userSettings'];
+  userSettings.$inject = ['COUCH'];
+  pouchCtrl.$inject = ['userSettings', '$scope'];
 
-  function runBootstrap(userSettings) {
+  function userSettings(COUCH) {
     
-    function dbSetup() {
-      var db = new PouchDB('foo');
-      
-      db.info().then(function (info) {
-        console.log('pouchDB info: ', info);
-      })
-    }
+    var db = new PouchDB(COUCH.host + COUCH.database);
     
-    userSettings.get();
-    dbSetup();
-  }
-  
-  function userSettings() {
-    function get() {
-      // get settings
+    function get(user) {
+      db.get(user).then(function (doc) {
+        console.log(doc);
+      });
     }
     
     function put(data) {
-      // put settings
+      db.get(data._id).then(function (doc) {
+        doc.options = data.options;
+        return db.put(doc);
+      }).then(function () {
+        return db.get(data._id);
+      }).then(function (doc) {
+        console.log(doc);
+      });
     }
     
     return {
@@ -33,14 +32,50 @@
     }
   }
   
-  function pouchCtrl() {
-    function saveSetting(data) {
-      // save settings using userSettings.put
-    }
+  function pouchCtrl(userSettings, $scope) {
+    
+    var vm = this;
+        
+    vm.user = {
+      _id: null,
+      options: [
+      { id: 0,
+        name: 'JavaScript',
+        selected: false },
+      { id: 1,
+        name: 'Python',
+        selected: false },
+      { id: 2,
+        name: 'Scala',
+        selected: false },
+    ]};
+    
+    // @todo - feel like there's a cleaner way
+    // to push things without user confirmation
+    // deep watch not performant
+    $scope.$watch(function() {
+      return vm.user.options;
+    }, function(newVal, oldVal) {
+      if (newVal !== oldVal) {
+        userSettings.put(vm.user);
+      }
+    }, true);
+    
+    $scope.$watch(function() {
+      return vm.user._id;
+    }, function(newVal, oldVal) {
+      if (newVal !== oldVal) {
+        userSettings.get(newVal);
+      }
+    })
+
   };
 
   angular.module('pouchDemo', [])
-    .run(runBootstrap)
+    .constant('COUCH', {
+      host: 'https://swirlycheetah.iriscouch.com/',
+      database: 'user_settings'
+    })
     .factory('userSettings', userSettings)
     .controller('pouchCtrl', pouchCtrl);
 
